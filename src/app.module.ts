31f99b43aaeb4fcdc8d32bfd3916a207346ab14d
greatSweetMango <jhkim0097@gmail.com>
2023-08-09 20:16:07 +0900

    why

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  // imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres', // Since we are using PostgreSQL.
+    host: 'localhost', // We are devoloping locally.
+    port: 5432, // What we set in our docker-compose file.
+    username: 'jaehyuki', // ""
+    password: '1234', // "pretty straightforward haha"
+    database: 'main', // db name.
+    autoLoadEntities: true, // help load entities automatically.
+    synchronize: true, // insures our entities are sync with the database every time we run our app.
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+// const ormconfig: TypeOrmModuleOptions = {
+//     "type": "postgres",
+//     "host": "localhost",
+//     "port": 5432,
+//     "username": "jaehyuki",
+//     "password": "1234",
+//     "database": "main",
+//     "synchronize": true,
+//     "logging": true,
+//     "entities": ["./src/**/*.entity.{.tx,.js}", "./dist/**/*.entity.{.ts,.js}"]
+// }
+// export default ormconfig
