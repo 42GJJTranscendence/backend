91671f5e4ff2618c9dd06c14266fa457f7a329ed
greatSweetMango <jhkim0097@gmail.com>
2023-08-09 17:42:21 +0900

    why

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot({
+    "type": "postgres",
+    "host": "postgresql",
+    "port": "5432",
+    "username": "jaehyuki",
+    "password": "1234",
+    "database": "MAIN",
+    "synchronize": "true",
+    "logging": "true",
+    "entities": ["./src/**/*.entity.{.tx,.js}", "./dist/**/*.entity.{.ts,.js}"]
+}),
+],
   controllers: [AppController],
   providers: [AppService],
 })
