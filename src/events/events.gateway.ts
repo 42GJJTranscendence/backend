a2a2fa04a292babb4acb8e1f70bac82cbee1b946
GihongYim <ygh0410@naver.com>
2023-08-26 08:58:24 +0000

    event.gateway.ts

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
-import { Server } from 'http';
-import { Observable, from, map } from 'rxjs';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway(8080)
+@WebSocketGateway(8080, { transports: ['websocket'] })
 export class EventsGateway {
-	@WebSocketServer()
-	server: Server;
+  @WebSocketServer()
+  server: Server;
 
-	@SubscribeMessage('events')
-	onEvent(client:any, data: any): Observable<WsResponse<number>> {
-		return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
-	}
+  @SubscribeMessage('ClientToServer')
+  async handleMessage(@MessageBody() data) {
+    console.log(data);
+    this.server.emit('ServerToClient', data);
+  }
 }
