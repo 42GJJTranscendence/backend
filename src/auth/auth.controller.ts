3b8261664af248bd41bc859a5d5ae922dacf70be
GihongYim <ygh0410@naver.com>
2023-09-13 15:58:35 +0900

    env changed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,116 +1,142 @@
-import { Body, Controller, Get, Post, Query, Req, Res, UseGuards } from "@nestjs/common";
-import { AuthService } from "./auth.service";
-import { LogInRequestDto, SignInRequestDto } from "./dto/auth.dto";
-import { AuthGuard } from "@nestjs/passport";
-import { User } from "src/modules/users/entity/user.entity";
-import { GetUser } from "./scurity/get-user.decorator";
-
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LogInRequestDto, SignInRequestDto } from './dto/auth.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/modules/users/entity/user.entity';
+import { GetUser } from './scurity/get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    constructor (
-        private authService : AuthService,
-    ){}
+  @Get('/42/oauth')
+  AuthRequest(@Res() res) {
+    res.redirect(
+      `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.UID_42}&redirect_uri=${process.env.CALLBACK_URI}&scope=public&response_type=code`,
+    );
+  }
 
-    @Get('/42/oauth')
-    AuthRequest(@Res() res) {
-        res.redirect (
-            `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.UID_42}&redirect_uri=${process.env.CALLBACK_URI}&scope=public&response_type=code`,
-        )
-    }
+  @Get('/42/callback')
+  async AuthRedirect(@Query('code') code: string, @Res() res) {
+    const fortyTwoToken = await this.authService.authenticate(code, res);
+    console.log(`${process.env.FRONT_SIGN_IN_URL}?token=${fortyTwoToken}`);
+    res.redirect(`${process.env.FRONT_SIGN_IN_URL}?token=${fortyTwoToken}`);
+  }
 
-    @Get('/42/callback')
-    async AuthRedirect(@Query('code') code:string, @Res() res) {
-        const fortyTwoToken = await this.authService.authenticate(code, res);
-        console.log(`${process.env.FRONT_SIGN_IN_URL}?token=${fortyTwoToken}`);
-        res.redirect(`${process.env.FRONT_SIGN_IN_URL}?token=${fortyTwoToken}`);
-    }
+  @Post('/signin')
+  async SignInUser(@Body() signInRequestDto: SignInRequestDto, @Res() res) {
+    console.log(
+      'username : ',
+      signInRequestDto.username,
+      '\npassword : ',
+      signInRequestDto.password,
+      '\neMail : ',
+      signInRequestDto.email,
+      '\nfortyTwoToken : ',
+      signInRequestDto.fortyTwoToken,
+    );
+    const jwtAccessToken = await this.authService.signInUser(signInRequestDto);
 
-    @Post('/signin')
-    async SignInUser(@Body() signInRequestDto : SignInRequestDto, @Res() res) {
-        console.log("username : ", signInRequestDto.username, "\npassword : ", signInRequestDto.password, "\neMail : ", signInRequestDto.email,"\nfortyTwoToken : ", signInRequestDto.fortyTwoToken);
-        const jwtAccessToken = await this.authService.signInUser(signInRequestDto);
+    // 쿠키 설정
+    const cookieOptions = {
+      httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
+      maxAge: 3600, // 쿠키 유효 기간 (초)
+      // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
+      // 다른 쿠키 옵션들도 설정 가능
+    };
 
-        // 쿠키 설정
-        const cookieOptions = {
-            httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
-            maxAge: 3600, // 쿠키 유효 기간 (초)
-            // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
-            // 다른 쿠키 옵션들도 설정 가능
-        };
+    console.log('ACCESS_TOKEN : ', jwtAccessToken);
+    res.send(jwtAccessToken);
+    // res.cookie('access_token', jwtAccessToken, cookieOptions);
 
-        console.log('ACCESS_TOKEN : ', jwtAccessToken);
-        res.send(jwtAccessToken);
-        // res.cookie('access_token', jwtAccessToken, cookieOptions);
+    // 리다이렉트
+    // res.redirect(`${process.env.FRONT_HOME_URL}`);
+  }
 
-        // 리다이렉트
-        // res.redirect(`${process.env.FRONT_HOME_URL}`);
-    }
+  @Post('/login')
+  async UserLogin(
+    @Body() logInRequestDto: LogInRequestDto,
+    @Res() res,
+  ): Promise<any> {
+    console.log(
+      'LogInRequestDto : {\n',
+      '\n    username: ',
+      logInRequestDto.username,
+      '\n  password: ',
+      logInRequestDto.password,
+    );
+    // 쿠키 설정
+    const cookieOptions = {
+      httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
+      maxAge: 3600, // 쿠키 유효 기간 (초)
+      // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
+      // 다른 쿠키 옵션들도 설정 가능
+    };
 
-    @Post('/login')
-    async UserLogin(@Body() logInRequestDto : LogInRequestDto, @Res() res) : Promise<any>{
-        console.log("LogInRequestDto : {\n", "\n    username: ", logInRequestDto.username, "\n  password: ", logInRequestDto.password);
-        // 쿠키 설정
-        const cookieOptions = {
-            httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
-            maxAge: 3600, // 쿠키 유효 기간 (초)
-            // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
-            // 다른 쿠키 옵션들도 설정 가능
-        };
+    const jwtAccessToken = await this.authService.validateUser(logInRequestDto);
 
-        const jwtAccessToken = await this.authService.validateUser(logInRequestDto);
+    console.log('ACCESS_TOKEN : ', jwtAccessToken);
+    res.send(jwtAccessToken);
 
-        console.log('ACCESS_TOKEN : ', jwtAccessToken);
-        res.send(jwtAccessToken);
+    res.cookie('access_token', jwtAccessToken, cookieOptions);
+    // 리다이렉트
+    return res.json(jwtAccessToken);
+    res.redirect(`${process.env.FRONT_HOME_URL}`);
+  }
 
-        res.cookie('access_token', jwtAccessToken, cookieOptions);
-        // 리다이렉트
-        return res.json(jwtAccessToken);
-        res.redirect(`${process.env.FRONT_HOME_URL}`);
-    }
+  @Get('/check/duplication')
+  async checkDuplication(@Query('username') username: string) {
+    return await this.authService.checkDuplication(username);
+  }
 
-    @Get('/check/duplication')
-    async checkDuplication(@Query('username') username : string){
-        return await this.authService.checkDuplication(username);
-    }
+  @Get('/cookie')
+  async CookieTest(@Res() res): Promise<any> {
+    // 쿠키 설정
+    const cookieOptions = {
+      httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
+      maxAge: 3600, // 쿠키 유효 기간 (초)
+      // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
+      // 다른 쿠키 옵션들도 설정 가능
+    };
 
-    @Get('/cookie')
-    async CookieTest(@Res() res) : Promise<any>{
-        // 쿠키 설정
-        const cookieOptions = {
-            httpOnly: true, // 클라이언트 스크립트로 쿠키 접근 불가
-            maxAge: 3600, // 쿠키 유효 기간 (초)
-            // secure: process.env.NODE_ENV === 'production', // HTTPS에서만 전송
-            // 다른 쿠키 옵션들도 설정 가능
-        };
+    const logInRequestDto = new LogInRequestDto();
+    logInRequestDto.username = 'jaehyuki';
+    logInRequestDto.password = '1234';
+    const jwtAccessToken = await this.authService.validateUser(logInRequestDto);
 
-        const logInRequestDto = new LogInRequestDto();
-        logInRequestDto.username = 'jaehyuki';
-        logInRequestDto.password = '1234';
-        const jwtAccessToken = await this.authService.validateUser(logInRequestDto);
+    console.log('ACCESS_TOKEN : ', jwtAccessToken);
 
-        console.log('ACCESS_TOKEN : ', jwtAccessToken);
+    res.cookie('access_token', jwtAccessToken, cookieOptions);
+    res.redirect(`${process.env.FRONT_HOME_URL}`);
+  }
 
-        res.cookie('access_token', jwtAccessToken, cookieOptions);
-        res.redirect(`${process.env.FRONT_HOME_URL}`);
-    }
+  @Get('/test')
+  @UseGuards(AuthGuard())
+  async AuthTest(@GetUser() user: User, @Res() res) {
+    console.log('req', user);
+    res.send(200, user.username);
+  }
 
-    @Get('/test')
-    @UseGuards(AuthGuard())
-    async AuthTest(@GetUser() user : User, @Res() res) {
-        console.log('req', user);
-        res.send(200, user.username);
-    }
+  @Get('/verification/email')
+  async sendVerificationMail(@Query('email') email: string, @Res() res) {
+    this.authService.sendVerificationCode(email);
+    res.send(200);
+  }
 
-    @Get('/verification/email')
-    async sendVerificationMail(@Query('email') email : string, @Res() res) {
-        this.authService.sendVerificationCode(email);
-        res.send(200);
-    }
-
-    @Get('/verification/email/check')
-    async checkVerificationMailCode(@Query('email') email : string, @Query('code') code : string) {
-        return await this.authService.checkVerificationCode(email, code);
-    }
+  @Get('/verification/email/check')
+  async checkVerificationMailCode(
+    @Query('email') email: string,
+    @Query('code') code: string,
+  ) {
+    return await this.authService.checkVerificationCode(email, code);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,156 +1,187 @@
-import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
-import { User } from "src/modules/users/entity/user.entity";
-import { FortyTwoUserDto, LogInRequestDto, SignInRequestDto, FortyTwoTokenJsonInterface } from "./dto/auth.dto";
-import { UserDto } from "src/modules/users/dto/user.dto";
-import { UserService } from "src/modules/users/service/user.service";
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { User } from 'src/modules/users/entity/user.entity';
+import {
+  FortyTwoUserDto,
+  LogInRequestDto,
+  SignInRequestDto,
+  FortyTwoTokenJsonInterface,
+} from './dto/auth.dto';
+import { UserDto } from 'src/modules/users/dto/user.dto';
+import { UserService } from 'src/modules/users/service/user.service';
 import * as bcrypt from 'bcrypt';
-import { Payload } from "./scurity/payload.interface";
-import { JwtService } from "@nestjs/jwt";
-import { UserDuplicatException } from "src/common/exception/custom.exception";
-import { MailerService } from "@nestjs-modules/mailer";
-import { RedisClientType } from "redis";
+import { Payload } from './scurity/payload.interface';
+import { JwtService } from '@nestjs/jwt';
+import { UserDuplicatException } from 'src/common/exception/custom.exception';
+import { MailerService } from '@nestjs-modules/mailer';
+import { RedisClientType } from 'redis';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @Inject('REDIS_CLIENT')
+    private readonly redis: RedisClientType,
+    private userService: UserService,
+    private jwtService: JwtService,
+    private readonly mailerService: MailerService,
+  ) {}
 
-    constructor(
-        @Inject('REDIS_CLIENT')
-        private readonly redis: RedisClientType,
-        private userService : UserService,
-        private jwtService : JwtService,
-        private readonly mailerService: MailerService,
-    ) {}
+  async sendVerificationCode(email: string): Promise<void> {
+    const code = Math.floor(Math.random() * 10000).toString();
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'FortyTwo Transcendence 인증 코드',
+      text: `Your verification code is ${code}`,
+    });
+    await this.redis.set(email, code, { EX: 300 });
+  }
 
-    async sendVerificationCode(email: string): Promise<void>{
-        const code = Math.floor(Math.random() * 10000).toString();
-        await this.mailerService.sendMail({
-            to: email,
-            subject: 'FortyTwo Transcendence 인증 코드',
-            text: `Your verification code is ${code}`,
-        });
-        await this.redis.set(email, code, { EX : 300 });
+  async checkVerificationCode(email: string, code: string): Promise<Boolean> {
+    const codeFind = await this.redis.get(email);
+    if (codeFind == null || codeFind != code) return false;
+    else {
+      await this.redis.del(email);
+      return true;
     }
+  }
 
-    async checkVerificationCode(email: string, code: string): Promise<Boolean> {
-        const codeFind = await this.redis.get(email);
-        if (codeFind == null || codeFind != code)
-            return false;
-        else {
-            await this.redis.del(email);
-            return true;
+  async getToken(code: string, res: Response): Promise<string> {
+    console.log('now retrieving token...');
+    const body = {
+      grant_type: 'authorization_code',
+      client_id: process.env.UID_42,
+      client_secret: process.env.SECRET_42,
+      redirect_uri: process.env.CALLBACK_URI,
+      code: code,
+    };
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+    console.log('Sending request:', options);
+    const token = await fetch('https://api.intra.42.fr/oauth/token', options)
+      .then(async (response) => {
+        const json = (await response.json()) as FortyTwoTokenJsonInterface;
+        console.log(json);
+        if (!response.ok) {
+          return Promise.reject(json.message);
         }
-    }
+        return json.access_token as string;
+      })
+      .catch((error) => {
+        console.log(error);
+        return '';
+      });
+    return Promise.resolve(token);
+  }
 
-    async getToken(code: string, res: Response): Promise<string> {
-        console.log("now retrieving token...");
-        const body = {
-            "grant_type": 'authorization_code',
-            "client_id": process.env.UID_42,
-            "client_secret": process.env.SECRET_42,
-            "redirect_uri": process.env.CALLBACK_URI,
-            "code": code,
+  async getUser(token: string): Promise<FortyTwoUserDto | undefined> {
+    //authenticate user with 42intra token
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    };
+    return fetch('https://api.intra.42.fr/v2/me', options)
+      .then(async (response) => {
+        if (!response.ok) {
+          return Promise.reject(
+            `Error ${response.status}: Failed to get user infos`,
+          );
         }
-        const options = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(body),
-        };
-        console.log("Sending request:", options);
-        const token = await fetch("https://api.intra.42.fr/oauth/token", options)
-            .then(async (response) => {
-                const json = await response.json() as FortyTwoTokenJsonInterface;
-                console.log(json);
-                if (!response.ok) {
-                    return Promise.reject(json.message);
-                }
-                return json.access_token as string;
-            })
-            .catch((error) => {
-                console.log(error);
-                return '';
-            });
-        return Promise.resolve(token);
+        const json = (await response.json()) as FortyTwoUserDto;
+        if (json.hasOwnProperty('id') && json.hasOwnProperty('login')) {
+          const id42 = json.id;
+          const username = json.login;
+          const imageUrl = json.image.link;
+          console.log(
+            'Get User\n{',
+            '\nid:',
+            id42,
+            '\nusername:',
+            username,
+            '\ntoken:',
+            token,
+            '\nimageUrl: ',
+            imageUrl,
+          ),
+            '\n}';
+          return json;
+        } else return undefined;
+      })
+      .catch((error) => {
+        console.log(error);
+        return undefined;
+      });
+  }
+
+  async authenticate(code: string, res: Response): Promise<String> {
+    const token = await this.getToken(code, res);
+    if (token == '') return undefined;
+    return token;
+  }
+
+  async signInUser(
+    signInRequestDto: SignInRequestDto,
+  ): Promise<String | undefined> {
+    if ((await this.checkDuplication(signInRequestDto.username)) == true) {
+      throw new UserDuplicatException();
+    }
+    const fortyTwoUserDto = await this.getUser(signInRequestDto.fortyTwoToken);
+    const user = new User();
+    user.username = signInRequestDto.username;
+    user.eMail = signInRequestDto.email;
+    user.imageUrl = fortyTwoUserDto.image.link;
+    user.fortyTwoId = fortyTwoUserDto.id;
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(
+      signInRequestDto.password,
+      saltRounds,
+    );
+    user.password = hashedPassword;
+
+    const createdUser = await this.userService.createUser(user);
+
+    const payload: Payload = {
+      id: createdUser.id,
+      username: createdUser.username,
+      fortyTwoId: createdUser.fortyTwoId,
+    };
+
+    return Promise.resolve(this.jwtService.sign(payload));
+  }
+
+  async validateUser(
+    logInRequestDto: LogInRequestDto,
+  ): Promise<string | undefined> {
+    let userFind: User = await this.userService.findOneByUsername(
+      logInRequestDto.username,
+    );
+    const validatePassword = await bcrypt.compare(
+      logInRequestDto.password,
+      userFind.password,
+    );
+    if (!userFind || !validatePassword) {
+      throw new UnauthorizedException();
     }
 
-    async getUser(token: string): Promise<FortyTwoUserDto | undefined> {
-        
-        //authenticate user with 42intra token
-        const options = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + token,
-            },
-        };
-        return fetch("https://api.intra.42.fr/v2/me", options)
-            .then(async (response) => {
-                if (!response.ok) {
-                    return Promise.reject(`Error ${response.status}: Failed to get user infos`);
-                }
-                const json = await response.json() as FortyTwoUserDto;
-                if (json.hasOwnProperty('id') && json.hasOwnProperty('login')) {
-                    const id42 = json.id;
-                    const username = json.login;
-                    const imageUrl = json.image.link;
-                    console.log("Get User\n{", "\nid:", id42, "\nusername:", username, "\ntoken:", token, "\nimageUrl: ", imageUrl), "\n}";
-                    return json;
-                } else
-                    return undefined;
-            })
-            .catch((error) => {
-                console.log(error);
-                return undefined;
-            });
+    const payload: Payload = {
+      id: userFind.id,
+      username: userFind.username,
+      fortyTwoId: userFind.fortyTwoId,
+    };
+
+    return Promise.resolve(await this.jwtService.sign(payload));
+  }
+
+  async checkDuplication(username: string): Promise<Boolean> {
+    let userFind: User = await this.userService.findOneByUsername(username);
+    if (userFind == null) return false;
+    else {
+      return true;
     }
-
-    async authenticate(code: string, res: Response): Promise<String> {
-        const token = await this.getToken(code, res);
-        if (token == '')
-            return undefined;
-        return token;
-    }
-
-    async signInUser(signInRequestDto : SignInRequestDto): Promise<String | undefined> {
-        if (await this.checkDuplication(signInRequestDto.username) == true) {
-            throw new UserDuplicatException();
-        }
-        const fortyTwoUserDto = await this.getUser(signInRequestDto.fortyTwoToken);
-        const user = new User();
-        user.username=signInRequestDto.username;
-        user.eMail=signInRequestDto.email;
-        user.imageUrl = fortyTwoUserDto.image.link;
-        user.fortyTwoId = fortyTwoUserDto.id;
-
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(signInRequestDto.password, saltRounds);
-        user.password = hashedPassword;
-
-        const createdUser = await this.userService.createUser(user);
-
-        const payload: Payload = { id: createdUser.id, username: createdUser.username, fortyTwoId: createdUser.fortyTwoId};
-        
-        return Promise.resolve(this.jwtService.sign(payload));
-    }
-
-    async validateUser(logInRequestDto: LogInRequestDto): Promise<string | undefined> {
-        let userFind: User = await this.userService.findOneByUsername(logInRequestDto.username);
-        const validatePassword = await bcrypt.compare(logInRequestDto.password, userFind.password);
-        if(!userFind || !validatePassword) {
-            throw new UnauthorizedException();
-        }
-    
-        const payload: Payload = { id: userFind.id, username: userFind.username, fortyTwoId: userFind.fortyTwoId};
-        
-        return Promise.resolve(await this.jwtService.sign(payload));
-    }
-
-    async checkDuplication(username : string): Promise<Boolean> {
-        let userFind: User = await this.userService.findOneByUsername(username);
-        if (userFind == null)
-            return false;
-        else
-        {
-            return true;
-        }
-    }
+  }
 }
