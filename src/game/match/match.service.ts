5b996a81b1169a9249409319c37428f8733f9874
greatSweetMango <jhkim0097@gmail.com>
2023-09-24 19:30:45 +0900

    good

--- src/game/match/match.service.ts
+++ src/game/match/match.service.ts
@@ -9,23 +9,26 @@ export class MatchService {
 
     constructor(
         @InjectRepository(Match)
-        private matchRepository : Repository<Match>,
-    ) {}
+        private matchRepository: Repository<Match>,
+    ) { }
 
-    async createMatch(match : Match) : Promise<Match | undefined> {
+    async createMatch(match: Match): Promise<Match | undefined> {
         const createMatch = await this.matchRepository.save(match);
         return createMatch;
     }
 
-    async getMatches(user: User) : Promise<Match[]> {
-        const matches = await this.matchRepository
-            .createQueryBuilder('match')
-            .where('match.userHomeIdId = :userId', { userId: user.id })
-            .orWhere('match.userAwayIdId = :userId', { userId: user.id })
-            .orderBy('match.start_at', 'DESC')
-            .getMany();
-    
+    async getMatches(user: User): Promise<Match[]> {
+        const matches = await this.matchRepository.find({
+            where: [{ userAwayId: user }, { userHomeId: user }],
+            order: {
+                start_at: 'DESC',
+            },
+            // relations: ['userHomeId, userAwayId']
+        })
+
+        console.log(matches);
+
         return matches;
     }
-    
+
 }
